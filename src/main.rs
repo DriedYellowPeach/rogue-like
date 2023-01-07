fcab1cd97dfff88ab925946302ce4a93a6d4ae00
neil <958862317@qq.com>
2023-01-07 14:17:28 +0800

    ch02: component Position and Renderable

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
-use rltk::{GameState, Rltk};
+use rltk::{GameState, Rltk, RGB};
+use specs::prelude::*;
+use specs_derive::Component;
 
 struct State {
-    height: i32,
-    width: i32,
-    welcome: String
+    ecs: World,
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        ctx.print(self.width / 2 - self.welcome.len() as i32 / 2, self.height / 2, self.welcome.to_owned());
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
+}
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
 }
 
 fn main() -> rltk::BError {
@@ -22,7 +40,36 @@ fn main() -> rltk::BError {
         .build()?;
 
     // gs ticks every tick, this will set the monitor terminal
-    let gs = State{height:50, width: 80, welcome: String::from("Hello From Neil!!!")};
+    let mut gs = State {
+        ecs: World::new()
+    };
+    
+    // let ecs register component
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    
+    // before main loop, create all the entity
+    gs.ecs
+        .create_entity()
+        .with(Position{x: 40, y: 25})
+        .with(Renderable{
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+    
+    for i in 1..=10 {
+        gs.ecs
+            .create_entity()
+            .with(Position{x: i * 7, y: 10})
+            .with(Renderable{
+                glyph: rltk::to_cp437('&'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .build();
+    }
 
     // game main loop, inside monitor, rendering by calling tick
     rltk::main_loop(monitor, gs)
